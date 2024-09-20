642aa605430cab91064dbe39e3b9daa798faf4e8
suvakantamalik <134865642+suvakantamalik@users.noreply.github.com>
2024-09-20 20:16:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function downloadImages(){
+	const downloadPromises = images.map(image => {
+		return new Promise((resolve, reject) => {
+			const img = new Image();
+		img.src = image.url;
+		
+		img.onload = () => resolve(img);
+
+		img.onerror = () => reject(new error(`Failed to load image's URL: ${image.url}`));
+		})	
+	});
+	
+}
+
+btn.addEventListener("click", downloadImages);
